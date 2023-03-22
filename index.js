1952b80d2c2e87658b4e8289e3d487f65f0dc9b8
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2023-03-22 23:01:31 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = "monica"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(name){
+    return bestCustomer = name
+}
+
+function changeLeastFavoriteCustomer(name){
+    return leastFavoriteCustomer = name
+}
+
+//console.log(changeLeastFavoriteCustomer("Ross"))
